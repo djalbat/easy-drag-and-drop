34c20e42c7cf5be8dff92766723e272d28412b90
James Smith <james.smith@djalbat.com>
2024-01-16 18:35:16 +0000

    Fixed the custom event types.

--- src/customEventTypes.js
+++ src/customEventTypes.js
@@ -2,10 +2,10 @@
 
 export const DRAG_CUSTOM_EVENT_TYPE = "drag-custom";
 export const DROP_CUSTOM_EVENT_TYPE = "drop-custom";
-export const DRAG_OUT_CUSTOM_EVENT_TYPE = "drag-out-custom";
-export const DRAG_OVER_CUSTOM_EVENT_TYPE = "drag-over-custom";
-export const STOP_DRAG_CUSTOM_EVENT_TYPE = "stop-drag-custom";
-export const START_DRAG_CUSTOM_EVENT_TYPE = "start-drag-custom";
+export const DRAG_OUT_CUSTOM_EVENT_TYPE = "dragout-custom";
+export const DRAG_OVER_CUSTOM_EVENT_TYPE = "dragover-custom";
+export const STOP_DRAG_CUSTOM_EVENT_TYPE = "stopdrag-custom";
+export const START_DRAG_CUSTOM_EVENT_TYPE = "startdrag-custom";
 
 export default {
   DRAG_CUSTOM_EVENT_TYPE,
